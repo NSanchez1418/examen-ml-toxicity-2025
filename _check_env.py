7a98a04d14c6e1642bd0f5634af37d2d8c6c4f58
NSanchez1418 <noe.sanchez@yachaytech.edu.ec>
2025-10-17 17:12:14 -0500

    Render Quarto: Tweets Classification (fix Pandoc + no embed)

---
+++ _check_env.py
@@ -0,0 +1,22 @@
+﻿import sys, subprocess
+
+print("python", sys.version)
+
+# --- NLTK stopwords ---
+import nltk
+try:
+    nltk.data.find('corpora/stopwords')
+    print('nltk stopwords: OK')
+except LookupError:
+    nltk.download('stopwords')
+    print('nltk stopwords: descargadas')
+
+# --- spaCy es_core_news_sm ---
+import spacy
+try:
+    spacy.load('es_core_news_sm')
+    print('spacy es_core_news_sm: OK')
+except OSError:
+    subprocess.run([sys.executable, '-m', 'spacy', 'download', 'es_core_news_sm'], check=True)
+    spacy.load('es_core_news_sm')
+    print('spacy es_core_news_sm: instalado')
